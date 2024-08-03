26482706be10d23a1fcd92be2ca35613d434aa26
ArcherXcode <sujeevgyawali@gmail.com>
2024-08-03 12:53:23 +0530

    First Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,30 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import {Tabs} from 'expo-router';
+import {View, Text, StyleSheet} from 'react-native';
+import { Entypo, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+// Import your tab screens here
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+const App = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen name="home" options={{tabBarIcon : () =>(
+                <Ionicons name='home-outline' size={20} color={'#000'}/>
+            ),
+            title: "Home",
+            }}/>
+            <Tabs.Screen name="classroom" options={{tabBarIcon : () =>(
+                <MaterialCommunityIcons name='google-classroom' size={20} color={'#000'}/>
+            ),
+            title: "Class",
+            }}/>
+            <Tabs.Screen name="attendance" options={{tabBarIcon : () =>(
+                <Entypo name='bar-graph' size={20} color={'#000'}/>
+            ),
+            title: "Attendance",
+            }}/>
+        </Tabs>
+    );
+};
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+export default App;
